37960bdd660083cfdd756df7e634603c99aa2162
Adria-Chiang <76279007+Adria-Chiang@users.noreply.github.com>
2022-10-05 15:23:51 -0700

    Add files via upload

---
+++ longest.py
@@ -0,0 +1,5 @@
+strs = ["flower","flow","flight"]
+
+for i in range(len(strs)):
+    for p in range(len(strs[i])):
+        print(strs[i][p])
---
+++ merge.py
@@ -0,0 +1,9 @@
+list1 = [1,2,4]
+list2 = [1,3,4]
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
